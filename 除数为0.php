dad580bdac05418d64278edef74a0f3ed457fe12
zzw <zzw.me@qq.com>
2019-06-21 20:52:35 +0800

    运算符

--- 除数为0.php
+++ 除数为0.php
@@ -29,3 +29,24 @@ var_dump($a == 'weekend' || $b == 'good');
 //逻辑非
 var_dump($b == 'good');
 var_dump(!($b == 'good'));
+
+echo '<hr/>';
+
+//连接运算符
+$a = 'hello';
+$b = 123;
+ echo $a . $b;
+
+ $a .= $b; //$a = $a . $b;
+echo $a;
+
+//错误抑制符
+$a = 10;
+$b = 0;
+//@($a % $b);
+
+//三木运算符
+echo '<hr/>';
+$a = 10;
+$b = $a>10 ? 100 : 0;
+echo $b;
