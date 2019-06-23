cf5141ac2441d15d9c83ed0e7807204a0b51e707
zzw <zzw.me@qq.com>
2019-06-23 10:45:22 +0800

    系统函数

---
+++ 打印输出.php
@@ -0,0 +1,17 @@
+<?php
+
+//系统函数
+
+//输出相关
+echo print ('hello world!<br/>');
+print ('hello world!<br/>');
+
+$a = 'hello world!<br/>';
+print_r($a);
+
+echo '<hr/>';
+
+//时间函数
+echo date('Y年 m月 d日 H:i:s',12345678),'<br/>';
+echo time(),'<br/>';
+echo microtime(),'<br/>';
