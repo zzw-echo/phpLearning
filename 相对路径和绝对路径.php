380edf7362f82e924826dfcb134af9fb9c5630fc
zzw <zzw.me@qq.com>
2019-06-23 19:43:04 +0800

    相对路径与绝对路径

---
+++ 相对路径和绝对路径.php
@@ -0,0 +1,14 @@
+<?php
+
+//php文件加载路径
+//include_once '文件包含.php';
+
+//include_once './文件包含.php';
+
+//相对路径的加载
+//include_once '../phpLearning/文件包含.php';
+
+//绝对路径
+include_once 'D:\workspace\phpws\phpLearning\文件包含.php';
+
+echo $a;
