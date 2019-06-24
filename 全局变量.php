043c48ade691ad54faad26d712f4194ec77081b9
zzw <zzw.me@qq.com>
2019-06-24 10:27:11 +0800

    全局变量

--- 全局变量.php
+++ 全局变量.php
@@ -3,15 +3,28 @@
 //全局变量不能访问局部变量
 //局部变量不能访问全局
 
-$global = 'global area';
+$global = 'global area';    //最终会被系统纳入到超全局变量中：$GLOBALS['global'] = global area;
 
 function display(){
     $inner = __FUNCTION__;
 
+//    var_dump($GLOBALS);
 //    echo $global;
+
+    //定义变量：使用全局变量
+    global $global;
+//    echo $global;
+
+    //定义变量：全局不存在
+    global $local;
+    $local = 'inner';
+
 }
 
 display();
 
+//全局空间访问局部变量
 //echo $inner;
 
+//全局空间访问“局部变量”
+//echo $local;
