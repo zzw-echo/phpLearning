a71b6b38f84d57f3935cb9fb85d49cd3e06cd70d
zzw <zzw.me@qq.com>
2019-06-24 11:01:03 +0800

    可变变量

--- 可变函数.php
+++ 可变函数.php
@@ -8,4 +8,20 @@ function display(){
 
 $func = 'display';
 
-$func();
+//$func();
+
+//定义系统函数（假设）
+function sys_fucntion($arg1, $arg2){
+    //给指定的函数（第一个参数），求对于的第二个参数的4次方
+    //为实际用户输入的数值进行处理
+    $arg2 = $arg2 + 10;
+    return $arg1($arg2);    //user_function(20)
+
+}
+
+//定义一个用户函数：求一个数的四次方
+function user_function($num){
+    return $num * $num * $num * $num;
+}
+
+echo sys_fucntion('user_function',10);
