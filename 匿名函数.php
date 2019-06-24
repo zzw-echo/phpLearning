6ff291c0054ca9525f606bf6cc178d83fb39fd43
zzw <zzw.me@qq.com>
2019-06-24 15:55:39 +0800

    匿名变量

--- 匿名函数.php
+++ 匿名函数.php
@@ -8,4 +8,24 @@ $func = function (){
 };
 
 //调用匿名函数
-$func;
+//var_dump($func);
+
+
+//闭包函数
+function display(){
+    //定义变量
+    $name = __FUNCTION__;
+
+    //定义匿名函数，通过use使用外部变量
+    $innerfunction = function () use($name){    //use就是将外部变量保留给内部使用
+        //函数内部的函数
+        echo $name;
+    };
+
+    //调用
+    $innerfunction();
+
+
+}
+
+display();
