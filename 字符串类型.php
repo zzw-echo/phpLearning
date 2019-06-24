49be2c3303be59a04a8381d54afa61fcefbfe004
zzw <zzw.me@qq.com>
2019-06-24 22:10:17 +0800

    字符串类型

--- 字符串类型.php
+++ 字符串类型.php
@@ -5,7 +5,7 @@
 //引号定义
 $str1 = 'hello';
 $str2 = "hello";
-var_dump($str1,$str2);
+//var_dump($str1,$str2);
 
 //结构化定义
 //heredoc结构
@@ -20,4 +20,19 @@ $str4 = <<<'EOD'
         world
 EOD;
 
-var_dump($str3,$str4);
+//var_dump($str3,$str4);
+
+//定义字符串转义符号
+$str5 = 'abc\r\ndef\t\'\"\$fg';
+$str6 = "abc\r\ndef\t\'\"\$fg";
+//echo $str5 ,'<br/>', $str6;
+
+//变量识别
+$a = 'hello';
+
+$str1 = 'abc $a def';
+$str2 = "abc $a def";
+//$str3 = "abc$adef";
+$str4 = "abc{$a}def";
+
+echo $str1,'<br/>',$str2,'<br/>',$str3,'<br/>',$str4;
