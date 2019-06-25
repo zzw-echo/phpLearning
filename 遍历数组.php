5619723c62aea568590d2cd5216146be573e1898
zzw <zzw.me@qq.com>
2019-06-25 21:40:56 +0800

    遍历数组

--- 遍历数组.php
+++ 遍历数组.php
@@ -13,3 +13,24 @@ echo '<pre>';
 //print_r(each($arr));
 //var_dump(each($arr));
 
+//list结构
+$arr = array(1, 2, 2=>3);
+
+list($first) = $arr;
+list($first, $a, $second) = $arr;
+//var_dump($first, $a, $second);
+
+//var_dump($first);
+//var_dump($first);
+//var_dump($first);
+
+//while循环
+$arr = array(1,'name'=>'Tom',3,'age'=>30);
+
+while (    list($key, $value) = each($arr)){
+    //list搭配each
+//    list($key, $value) = each($arr);
+
+    //输出
+    echo 'key is ' . $key. ' value is '. $value. '<br/>';
+}
