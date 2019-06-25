8dc3f8dea261d163fd9cf6bcc2f711ec4af68859
zzw <zzw.me@qq.com>
2019-06-25 16:15:13 +0800

    多维数组

---
+++ 多维数组.php
@@ -0,0 +1,14 @@
+<?php
+
+//多维数组
+
+//定义二维数组
+$info = array(
+    array('name' => 'Jim','age' => 30),
+    array('naem' => 'Tom','age' => 28),
+    array('name' => 'Lily','age' => 20)
+);
+
+echo '<pre>';
+
+print_r($info);
