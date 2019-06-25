e5559e92fa548bf823a63880746abe41e4b31891
zzw <zzw.me@qq.com>
2019-06-25 21:21:43 +0800

    遍历数组

---
+++ 遍历数组.php
@@ -0,0 +1,15 @@
+<?php
+
+//while配合each和list遍历数组
+
+$arr = array(1,'name'=>'Tom',3,'age'=>30);
+
+echo '<pre>';
+
+//each函数指针操作
+//print_r(each($arr));
+//print_r(each($arr));
+//print_r(each($arr));
+//print_r(each($arr));
+//var_dump(each($arr));
+
