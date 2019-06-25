ee74044a639ccc28f182f21002acf20e9888d71c
zzw <zzw.me@qq.com>
2019-06-25 21:47:36 +0800

    数组排序

---
+++ 数组排序.php
@@ -0,0 +1,21 @@
+<?php
+
+
+//PHP数组相关函数
+$arr = array(3,1,5,7,2);
+
+echo '<pre>';
+
+sort($arr);
+
+//asort($arr);
+//krsort($arr);
+
+shuffle($arr);
+print_r($arr);
+
+
+shuffle($arr);
+print_r($arr);
+
+
