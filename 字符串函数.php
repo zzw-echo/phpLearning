1ee17aaf8dcfa5779bb6e806f1cf6b58b6cb1aa7
zzw <zzw.me@qq.com>
2019-06-25 15:35:00 +0800

    字符串函数

--- 字符串函数.php
+++ 字符串函数.php
@@ -8,5 +8,24 @@ $str = '   abc  d ef  g   ';
 
 
 //字符串截取
-echo substr($str,1,3),'<br/>';
-echo strstr($str,'c');
+//echo substr($str,1,3),'<br/>';
+//echo strstr($str,'c');
+
+
+$str = '123a234a4b2a';
+
+//查找位置
+//echo strpos($str,'a'),'<br/>';
+//echo strrpos($str,'a'),'<br/>';
+
+//echo str_replace('a','b',$str);
+
+$age = 50;
+$name = 'Tom';
+//格式化输出
+//echo sprintf("你好，今年我%d岁，我叫%s", $age, $name);
+
+$str3 = 'abcdefg';
+echo str_repeat($str3,5),'<br/>';
+
+echo str_shuffle($str3);
