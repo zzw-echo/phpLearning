a08e39253f0e28cb556e7b2e42897b875e015644
zzw <zzw.me@qq.com>
2019-06-26 12:19:00 +0800

    选择排序

---
+++ 选择排序.php
@@ -0,0 +1,22 @@
+<?php
+
+$arr = array(1,4,2,9,7,5,8);
+
+for ($i = 0,$len = count($arr); $i<$len; $i++){
+
+    $min = $i;
+    for ($j = $i+1; $j<$len ;$j++){
+        if ($arr[$j] < $arr[$min]){
+            $min = $j;
+        }
+    }
+
+    if ($min != $i){
+        $arr[$min] = $arr[$min] ^ $arr[$i];
+        $arr[$i] = $arr[$min] ^ $arr[$i];
+        $arr[$min] = $arr[$min] ^ $arr[$i];
+    }
+
+}
+echo '<pre>';
+print_r($arr);
