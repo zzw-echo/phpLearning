e840e2b19a5d483d1ed0841cd53d0d2f7f4e4677
zzw <zzw.me@qq.com>
2019-06-26 11:12:10 +0800

    匿名变量

--- 递推.php
+++ 递推.php
@@ -2,6 +2,7 @@
 
 
 //递推思想
+//递推算法求斐波那契数列
 
 $f[1] = 1;
 $f[2] = 1;
@@ -15,3 +16,21 @@ for ($i = 3; $i <=15; $i++){
 echo '<pre>';
 
 print_r($f);
+echo '<hr/>';
+
+function my_recursive($des){
+
+    if ($des == 1 || $des == 2) return 1;
+
+    $f[1] = 1;
+    $f[2] = 1;
+
+    for ($i = 3; $i <=15; $i++){
+        $f[$i] = $f[$i-1] + $f[$i-2];
+    }
+
+    return $f[$des];
+
+}
+
+echo my_recursive(15);
