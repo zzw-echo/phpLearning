93441bfa417c7513d059c8d474b193498be24924
zzw <zzw.me@qq.com>
2019-06-26 09:40:08 +0800

    数组排序

--- 数组排序.php
+++ 数组排序.php
@@ -12,10 +12,22 @@ sort($arr);
 //krsort($arr);
 
 shuffle($arr);
-print_r($arr);
+//print_r($arr);
 
 
 shuffle($arr);
-print_r($arr);
+//print_r($arr);
 
 
+
+$arr = array(3,1,5,7,2);
+
+//指针函数
+echo current($arr),'<br/>';
+echo key($arr),'<br/>';
+
+echo next($arr),next($arr),'<br/>';
+echo prev($arr),'<br/>';
+
+echo end($arr),'<br/>';
+echo reset($arr),'<br/>';
