6526af4ebfa39495f33952ff3c0ba1c7882ca0d5
zzw <zzw.me@qq.com>
2019-06-27 10:45:36 +0800

    归并排序

---
+++ 归并排序.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$arr = array(4,7,2,1,5,9,3);
+
+function merge_sort($arr){
+
+    $len = count($arr);
+    if ($len <= 1) return $arr;
+
+    //拆分
+    $middle = floor($len/2);
+    $left = array_slice($arr,0,$middle);
+    $right = array_slice($arr,$middle);
+
+    //假设左边和右边都已排好序：二路归并
+    $m = array();
+    while(count($left) && count($right)){
+
+        $m[] = $left[0] < $right[0] ? array_shift($left) : array_shift($right);
+    }
+
+    return array_merge($m,$left,$right);
+
+
+}
+
+print_r(merge_sort($arr));
