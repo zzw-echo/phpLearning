a496b07c936ea7d2804cdf6d20e5758f3b97fdcd
zzw <zzw.me@qq.com>
2019-07-07 13:54:47 +0800

    thinkPHP5.0.24框架

--- tp5/application/config.php
+++ tp5/application/config.php
@@ -140,6 +140,9 @@ return [
         'taglib_begin' => '{',
         // 标签库标签结束标记
         'taglib_end'   => '}',
+        'layout_on' => true,
+        'layout_name' => 'layout',
+        'layout_item' => '{__REPLACE__}',
     ],
 
     // 视图输出字符串内容替换
