5514bb5ad3750a8545a114a08d9d669a78f83173
zzw <zzw.me@qq.com>
2019-07-13 10:19:38 +0800

    thinkPHP5.0.24框架

--- tp5/application/index/controller/Detail.php
+++ tp5/application/index/controller/Detail.php
@@ -4,16 +4,16 @@ namespace app\index\controller;
 Class Detail{
 
     public function getInfo(){
-
+        dump('getInfo');
     }
     public function postInfo(){
-
+        dump('postInfo');
     }
     public function putInfo(){
-
+        dump('putInfo');
     }
     public function deleteInfo(){
-
+        dump('deleteInfo');
     }
 
 
