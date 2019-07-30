919f6fccfabebd78c18d989474616aa3abb16a6f
zzw <zzw.me@qq.com>
2019-07-30 12:22:59 +0800

    thinkPHP CRUD项目

--- tp5crud/application/admin/controller/Index.php
+++ tp5crud/application/admin/controller/Index.php
@@ -1,5 +1,8 @@
 <?php
 namespace app\admin\controller;
+
+
+
 use think\Controller;
 use app\index\model\Person as PersonModel;
 use app\index\validate\Person as PersonValidate;
@@ -12,8 +15,28 @@ class Index extends Controller{
 
     public function check(){
         $data = input('post.');
-        dump($data);
-        die;
+        $user = new \app\index\controller\User();
+        $result = $user->where('name',$data['name'])->find();
+        if ($result){
+            if ($result['password'] === md5($data['password'])){
+                session('name',$data['name']);
+            } else{
+                $this->error('密码错误');
+            }
+        }else{
+            $this->error('用户名不存在');
+            exit();
+        }
+        if (captcha_check($data['code'])){
+            $this->success('登录成功','User/Index');
+        }else{
+            $this->error('验证码错误');
+        }
+    }
+
+    public function logout(){
+        session(null);
+        $this->success('推出登录成功','Index/login');
     }
 
     public function index(){
--- tp5crud/application/admin/controller/User.php
+++ tp5crud/application/admin/controller/User.php
@@ -4,10 +4,108 @@ use think\Controller;
 use app\admin\model\User as UserModel;
 use app\admin\validate\User as UserValidate;
 
-class User extends Controller{
+class User extends Base{
     public function index(){
         return $this->fetch();
     }
+
+    //用户列表
+    public function userlist(){
+        //显示所有数据
+        /*
+        $data = UserModel::all();
+        $this->assign('data',$data);
+        return $this->fetch();
+        */
+
+        //分页显示数据
+        $data = UserModel::paginate(3);
+        $page = $data->render();
+        $this->assign('data',$data);
+        $this->assign('page',$page);
+        return $this->fetch();
+    }
+
+    public function add1(){
+        return $this->fetch();
+    }
+
+    public function edit1(){
+        $id = input('get.id');
+        $m = \app\admin\model\User();
+        $data = UserModel::get($id);
+        $this->assign('data', $data);
+        return $this->fetch();
+    }
+
+    /**
+     * 新增管理员的方法
+     */
+    public function insert1(){
+        $data = input('post.');
+
+        $val = new UserValidate();
+        if (!$val-check($data)){
+            $this->error($val->getError());
+            exit();
+        }
+
+        $user = new UserModel($data);
+        $ret = $user->allowField(true)->save();
+        if ($ret){
+            $this->success('新增管理员成功','User/userlist');
+        } else{
+            $this->error('新增管理员失败');
+        }
+    }
+
+    /**
+     * 提交用户信息更新
+     */
+    public function update1(){
+        $data = input('post.');
+        $id = input('post.id');
+        $val = new UserValidate();
+        if (!$val->check($data)){
+            $this->error($val->getError());
+            exit();
+        }
+        $user = new UserModel();
+        $ret = $user->allowField(true)->save($data, ['id' => $id]);
+        if ($ret){
+            $this->success('修改用户信息成功', 'User/userlist');
+        } else{
+            $this->error('修改用户信息失败');
+        }
+    }
+
+    /**
+     * 删除用户信息
+     */
+    public function delete1(){
+        //实现软删除的方法
+        $id = input('get.id');
+        $ret = UserModel::destroy($id);
+        if ($ret){
+            $this->success('用户删除成功', 'User/userlist');
+        } else{
+            $this->error('删除用户失败');
+        }
+
+        /*
+        //实现真实删除
+        $id = input('get.id');
+        $ret = UserModel::destroy($id, true);
+        if ($ret){
+            $this->success('删除用户成功','User/userlist');
+        }else{
+            $this->error('删除用户失败');
+        }
+        */
+
+    }
+
+
 
     public function list1(){
         /*
